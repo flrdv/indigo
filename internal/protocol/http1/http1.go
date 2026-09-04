508bcd4d43589149e6a7313d272c60203bb069f8
Pavlo <real.floordiv@gmail.com>
2026-09-04 17:40:13 +0200

    rename Suit to HTTP1, add safeguard against disabled protocols

--- internal/protocol/http1/http1.go
+++ internal/protocol/http1/http1.go
@@ -13,29 +13,29 @@ import (
 	"github.com/indigo-web/indigo/transport"
 )
 
-type Suit struct {
+type HTTP1 struct {
 	*Parser
-	*body
+	*Body
 	*serializer
+	client *Client
 	router router.Router
-	client transport.Client
 	codecs codecutil.Cache
 }
 
-func newSuit(
+func newHTTP1(
 	cfg *config.Config,
 	r router.Router,
 	request *http.Request,
-	client transport.Client,
-	body *body,
+	client *Client,
+	body *Body,
 	codecs codecutil.Cache,
 	statusBuff, headersBuff *buffer.Buffer,
 	respBuff []byte,
-) *Suit {
-	return &Suit{
+) *HTTP1 {
+	return &HTTP1{
 		Parser:     NewParser(cfg, request, statusBuff, headersBuff),
-		body:       body,
-		serializer: newSerializer(cfg, request, client, codecs, respBuff),
+		Body:       body,
+		serializer: newSerializer(cfg, request, client.Conn, codecs, respBuff),
 		router:     r,
 		client:     client,
 		codecs:     codecs,
@@ -49,39 +49,47 @@ func New(
 	client transport.Client,
 	request *http.Request,
 	codecs codecutil.Cache,
-) *Suit {
+) *HTTP1 {
 	statusBuff, headersBuff := construct.Buffers(cfg)
+	reqBuff := make([]byte, cfg.HTTP1.ReadBuffer)
+	c := NewClient(client, reqBuff)
+	b := NewBody(c, cfg.Body)
 	respBuff := make([]byte, 0, cfg.NET.WriteBufferSize.Default)
-	b := newBody(client, cfg.Body)
 
-	return newSuit(cfg, r, request, client, b, codecs, statusBuff, headersBuff, respBuff)
+	return newHTTP1(cfg, r, request, c, b, codecs, statusBuff, headersBuff, respBuff)
 }
 
-func (s *Suit) ServeOnce() (ok bool) {
-	return s.serve(true)
+func (h *HTTP1) ServeOnce() (ok bool) {
+	return h.serve(true)
 }
 
-func (s *Suit) Serve() {
-	s.serve(false)
+func (h *HTTP1) Serve() {
+	h.serve(false)
 }
 
-func (s *Suit) serve(once bool) (ok bool) {
-	client := s.client
-	request := s.Parser.request
+func (h *HTTP1) serve(once bool) (ok bool) {
+	client := h.client
+	request := h.Parser.request
 
 	for {
-		data, err := client.Read()
+		data, err := client.Fetch()
 		if err != nil {
 			// read-error most probably means deadline exceeding. Just notify the user in
 			// this case and return.
-			s.router.OnError(request, status.ErrCloseConnection)
+			h.router.OnError(request, status.ErrCloseConnection)
 			return false
 		}
 
-		done, extra, err := s.Parse(data)
+		done, bodydata, err := h.Parse(data)
 		if err != nil {
-			resp := respond(request, s.router.OnError(request, err))
-			_ = s.Write(request.Protocol, resp)
+			resp := respond(request, h.router.OnError(request, err))
+			_ = h.Write(request.Protocol, resp)
+			return false
+		}
+
+		if h.Parser.cfg.NET.Protocols&request.Protocol == 0 {
+			resp := respond(request, h.router.OnError(request, status.ErrHTTPVersionNotSupported))
+			_ = h.Write(request.Protocol, resp)
 			return false
 		}
 
@@ -93,64 +101,64 @@ func (s *Suit) serve(once bool) (ok bool) {
 			continue
 		}
 
-		client.Pushback(extra)
+		client.Pushback(bodydata)
 		request.Body.Reset(request)
-		s.body.Reset(request)
+		h.Body.Reset(request)
 
 		transferEncoding := request.TransferEncoding
 		if !validateTransferEncodingTokens(transferEncoding) {
-			resp := respond(request, s.router.OnError(request, status.ErrUnsupportedEncoding))
-			_ = s.Write(request.Protocol, resp)
+			resp := respond(request, h.router.OnError(request, status.ErrUnsupportedEncoding))
+			_ = h.Write(request.Protocol, resp)
 			return false
 		}
 
 		if len(transferEncoding) > 0 {
 			// get rid of the trailing chunked encoding as it is already built-in.
-			if err = s.applyDecoders(transferEncoding[:len(transferEncoding)-1]); err != nil {
+			if err = h.applyDecoders(transferEncoding[:len(transferEncoding)-1]); err != nil {
 				// even if the connection is going to be upgraded in advance, the error happened with the
 				// request prior to upgrade.
-				resp := respond(request, s.router.OnError(request, err))
-				_ = s.Write(request.Protocol, resp)
+				resp := respond(request, h.router.OnError(request, err))
+				_ = h.Write(request.Protocol, resp)
 				return false
 			}
 		}
 
-		if err = s.applyDecoders(request.ContentEncoding); err != nil {
-			resp := respond(request, s.router.OnError(request, err))
-			_ = s.Write(request.Protocol, resp)
+		if err = h.applyDecoders(request.ContentEncoding); err != nil {
+			resp := respond(request, h.router.OnError(request, err))
+			_ = h.Write(request.Protocol, resp)
 			return false
 		}
 
 		version := request.Protocol
 		if request.Upgrade != proto.Unknown && proto.HTTP1&request.Upgrade != 0 {
-			s.Upgrade()
+			h.Upgrade()
 			version = request.Upgrade
 		}
 
-		resp := respond(request, s.router.OnRequest(request))
+		resp := respond(request, h.router.OnRequest(request))
 
 		if request.Hijacked() {
 			// in case the connection was hijacked, we must not intrude after, so fail fast
 			return false
 		}
 
-		if err = s.Write(version, resp); err != nil {
+		if err = h.Write(version, resp); err != nil {
 			// considering any write errors could occur due to broken connection, it makes
 			// thereby no sense to try to write any error back. Moreover, there could be an
 			// already sent data, which would overlay and result in a complete mess at the
 			// client side.
-			s.router.OnError(request, status.ErrCloseConnection)
+			h.router.OnError(request, status.ErrCloseConnection)
 			return false
 		}
 
 		if err = request.Body.Discard(); err != nil {
-			resp = s.router.OnError(request, status.ErrCloseConnection)
-			_ = s.Write(request.Protocol, resp)
+			resp = h.router.OnError(request, status.ErrCloseConnection)
+			_ = h.Write(request.Protocol, resp)
 			return false
 		}
 
 		if !isKeepAlive(version, request) {
-			s.router.OnError(request, status.ErrCloseConnection)
+			h.router.OnError(request, status.ErrCloseConnection)
 			return true
 		}
 
@@ -190,12 +198,12 @@ func validateTransferEncodingTokens(tokens []string) bool {
 	return tokens[len(tokens)-1] == "chunked"
 }
 
-func (s *Suit) applyDecoders(tokens []string) error {
-	request := s.Parser.request
-	bufferSize := s.Parser.cfg.NET.ReadBufferSize
+func (h *HTTP1) applyDecoders(tokens []string) error {
+	request := h.Parser.request
+	bufferSize := h.Parser.cfg.HTTP1.ReadBuffer
 
 	for i := len(tokens); i > 0; i-- {
-		c := s.codecs.Get(tokens[i-1])
+		c := h.codecs.Get(tokens[i-1])
 		if c == nil {
 			return status.ErrUnsupportedEncoding
 		}
@@ -210,7 +218,7 @@ func (s *Suit) applyDecoders(tokens []string) error {
 	return nil
 }
 
-// respond ensures the passed resp is not nil, otherwise http.Respond(req) is returned
+// respond ensures the passed resp is not nil.
 func respond(req *http.Request, resp *http.Response) *http.Response {
 	if resp != nil {
 		return resp
